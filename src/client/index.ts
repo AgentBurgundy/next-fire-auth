a133f9cb3e1c20c08ae3d213d73c60e3ba88220b
ronald-onesta <ronald@onestafinance.com>
2023-10-04 13:41:43 -0500

    fixed errors

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,10 +1,10 @@
-import { NextFireAuthContextProvider } from "next-fire-auth/context/NextFireAuthContext";
-import { DefaultCookieManager } from "next-fire-auth/cookies/DefaultCookieManager";
-import { CookieManager } from "next-fire-auth/cookies/types/CookieManager";
-import { useAuthContext } from "next-fire-auth/hooks/client/useAuthContext";
-import { NextFireAuthConfig } from "next-fire-auth/types/NextFireAuthConfig";
-import { NextFireAuthContextProviderProps } from "next-fire-auth/types/NextFireAuthContextProviderProps";
-import { NextFireAuthContextType } from "next-fire-auth/types/NextFireAuthContextType";
+import { NextFireAuthContextProvider } from "@/context/NextFireAuthContext";
+import { DefaultCookieManager } from "@/cookies/DefaultCookieManager";
+import { CookieManager } from "@/cookies/types/CookieManager";
+import { useAuthContext } from "@/hooks/client/useAuthContext";
+import { NextFireAuthConfig } from "@/types/NextFireAuthConfig";
+import { NextFireAuthContextProviderProps } from "@/types/NextFireAuthContextProviderProps";
+import { NextFireAuthContextType } from "@/types/NextFireAuthContextType";
 
 export {
   NextFireAuthContextProvider,
