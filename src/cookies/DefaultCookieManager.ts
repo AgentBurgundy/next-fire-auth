aa66bca7782b76a4e381e0711e0b7dbdeb65a5cc
ronald-onesta <ronald@onestafinance.com>
2023-10-04 15:38:57 -0500

    secured cookie

--- src/cookies/DefaultCookieManager.ts
+++ src/cookies/DefaultCookieManager.ts
@@ -2,7 +2,7 @@ import Cookies from "js-cookie";
 
 export const DefaultCookieManager = {
   addAuthCookie: (uid: string) => {
-    Cookies.set("uid", uid, { expires: 7 });
+    Cookies.set("uid", uid, { secure: true });
   },
   removeAuthCookie: () => {
     Cookies.remove("uid");
